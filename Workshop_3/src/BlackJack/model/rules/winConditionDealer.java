0f97718caa54bf945d15246b1462ea9dfc0fcaa5
ma223im <ma223im@student.lnu.se>
2016-10-27 16:50:09 +0200

    win

---
+++ Workshop_3/src/BlackJack/model/rules/winConditionDealer.java
@@ -0,0 +1,10 @@
+package BlackJack.model.rules;
+
+public class winConditionDealer implements IWinCondition{
+
+	public boolean winCondition(int dealer_score, int player_score) {
+		
+		return true;
+	}
+
+}
